5b7175f98c03b01f903be04df994c08c5bf51cd6
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-06-17 20:04:00 -0500

    Threw IGDB table in schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,14 +1,15 @@
 DROP DATABASE IF EXISTS gnarly_games;
 CREATE DATABASE gnarly_games;
 
--- CheapShark Table
-CREATE TABLE price (
-  -- Go back to title.. link IGDB title(?)
+-- IGDB 
+CREATE TABLE game (
   id INT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salePrice INT NOT NULL,
-  storeId INT NOT NULL,
-  storeName VARCHAR(50) NOT NULL,
-  dealId INT NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  artwork VARCHAR(100) NOT NULL,
+  gameImages image,
+  rating VARCHAR(25) NOT NULL,
+  genre VARCHAR(25) NOT NULL,
+  trailer VARCHAR(100) NOT NULL,
+  reviews VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
