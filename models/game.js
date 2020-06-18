fce4347049f026e304c0830d91712894b2389a57
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-06-17 20:03:42 -0500

    Create game model

---
+++ models/game.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+  var Game = sequelize.define("Game", {
+    title: DataTypes.STRING,
+    artwork: DataTypes.TEXT,
+    rating: DataTypes.STRING,
+    description: DataTypes.TEXT
+  });
+
+  return Game;
+};
