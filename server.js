47a61703b90bf0ab34d3c202f36af821bba67255
El Diablo <estesquefawer@gmail.com>
2020-06-18 19:20:41 -0500

    add log in form

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.set('view engine', 'html');
 
 // Routes
 require("./routes/apiRoutes")(app);
