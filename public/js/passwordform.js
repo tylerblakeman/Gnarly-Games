8ce8e204daedfdfd6f21b5d41eb622ee1d5e3b34
El Diablo <estesquefawer@gmail.com>
2020-06-20 21:51:59 -0500

    login modiffication

---
+++ public/js/passwordform.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+// Getting information from the user "Login"
+var emailInputLi = $("#emailLogin");
+var passInputLi = $("#passwordLogin");
+
+var emailInputRe = $("#emailReg");
+var passwordRe = $("#passwordReg");
+
+// Adding event listeners to the form to create a new object
+$(document).on("click", "#login", function(){
+var username = $("#emailLogin").val()
+console.log(username);
+// console.log("hi");
+});
+});
