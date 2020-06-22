1b5c4e877ccced6b533ea5ae12cba49a64378c40
Firecraker <mezamic000@gmail.com>
2020-06-21 21:19:59 -0500

    added modal html to new handlebar page

--- public/js/games.js
+++ public/js/games.js
@@ -1,18 +1,15 @@
-$(document).ready(function(){
-  var gameName = $("#game-title");
-  var gameArtwork = $("#artwork");
-  var summary = $("#game-summary");
-
-  var IgdbURL = "https://api-v3.igdb.com/";
-
-  function gameInfo() {
-    event.preventDefault();
-  
-    $.ajax({
-      url: IgdbURL + "games" 
-      method: "GET"
-    }).then(function(data) {
-      console.log(data);
-    });
-  }
-})
+// $(document).ready(function () {
+//   var gameName = $("#game-title");
+//   var gameArtwork = $("#artwork");
+//   var summary = $("#game-summary");
+//   var IgdbURL = "https://api-v3.igdb.com/";
+//   function gameInfo() {
+//     event.preventDefault();
+//     $.ajax({
+//       url: IgdbURL + "games"
+//       method: "GET"
+//     }).then(function(data) {
+//       console.log(data);
+//     });
+//   }
+// });
--- public/js/passwordform.js
+++ public/js/passwordform.js
@@ -1,16 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Getting information from the user "Login"
+  var emailInputLi = $("#emailLogin");
+  var passInputLi = $("#passwordLogin");
 
-// Getting information from the user "Login"
-var emailInputLi = $("#emailLogin");
-var passInputLi = $("#passwordLogin");
+  var emailInputRe = $("#emailReg");
+  var passwordRe = $("#passwordReg");
 
-var emailInputRe = $("#emailReg");
-var passwordRe = $("#passwordReg");
-
-// Adding event listeners to the form to create a new object
-$(document).on("click", "#login", function(){
-var username = $("#emailLogin").val()
-console.log(username);
-// console.log("hi");
-});
+  // Adding event listeners to the form to create a new object
+  $(document).on("click", "#login", function() {
+    var username = $("#emailLogin").val();
+    console.log(username);
+    console.log("hi");
+  });
 });
